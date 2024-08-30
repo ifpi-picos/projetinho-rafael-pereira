c9c3b7e2983626fb157770ae3911ca21d4ef4189
Rafaelp05 <128104639+Rafaelp05@users.noreply.github.com>
2024-08-30 13:59:06 -0300

    Delete GerenciadorDeTarefas directory

--- GerenciadorDeTarefas/functions/3 - editarTarefa.js
+++
@@ -1,6 +0,0 @@
-export function editarTarefa(listaTarefas){
-    console.table(listaTarefas)
-    let escolha = Number(prompt('Qual tarefa deseja editar de acordo com o index?'))
-    const tarefa = listaTarefas[escolha]
-    let escolher = Number(prompt('1 - Nome \n2 - Descrição \n3 - Vencimento'))   
-}
--- GerenciadorDeTarefas/functions/5 - marcarConcluida.js
+++
@@ -1,9 +0,0 @@
-export function marcarConcluida(listaTarefas, tarefasConcluidas){
-    for(let tarefa of listaTarefas){
-        console.log(tarefa.nome)
-    }
-    let opcao = prompt('Qual tarefa deseja remover?').toLowerCase()
-    let feito = listaTarefas.findIndex((tarefa) => tarefa.nome === opcao)
-    let marcar = listaTarefas.splice(feito, 1)
-    tarefasConcluidas.push(marcar)
-}

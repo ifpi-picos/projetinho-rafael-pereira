1815b78c9b3c8893683a74d08cc204c3c86cba27
Rafaelp05 <pereiraborgesrafael05@gmail.com>
2024-08-31 15:41:52 -0300

    Atualização

--- GestorListaDeCompra/functions/3-editarItem.js
+++ GestorListaDeCompra/functions/3-editarItem.js
@@ -3,17 +3,24 @@ export function editarItem(listaItens){
     let escolha = prompt('Qual item deseja editar?').trim()
     let itemEscolhido = listaItens.find((item) => item.nome === escolha)
     if(itemEscolhido){
-        let novoNome = prompt('Qual o novo nome?')
+        let novoNome = prompt('Qual o novo nome?').trim()
         let novaQuantidade = Number(prompt('Qual a nova quantidade?'))
-        let novaCategoria = prompt('Qual a nova categoria?')
+        let novaCategoria = prompt('Qual a nova categoria?').trim()
+        while(novoNome === '' || Number(novaQuantidade) === '' || novaCategoria === ''){
+            if(novoNome === ''){
+                novoNome = prompt('Qual o novo nome?').trim()
+            }
+            if(Number(novaQuantidade) === ''){
+                novaQuantidade = prompt('Qual a nova quantidade?')
+            }
+            if(novaCategoria === ''){
+                novaCategoria = prompt('Qual a nova categoria?').trim()
+            }
+        }
 
         itemEscolhido.nome = novoNome
         itemEscolhido.quantidade = novaQuantidade
         itemEscolhido.categoria = novaCategoria
 
-        console.log(itemEscolhido)
-    }
-    else{
-        console.log('Item não encontrado')
     }
 }
